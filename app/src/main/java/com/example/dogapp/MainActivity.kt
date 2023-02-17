29d70df5711732514596e062d8021f7d5ac16b55
R0dr1g3z <r0dr1g3zek@gmail.com>
2023-02-17 15:21:14 +0100

    update Dog App

--- app/src/main/java/com/example/dogapp/MainActivity.kt
+++ app/src/main/java/com/example/dogapp/MainActivity.kt
@@ -1,15 +1,32 @@
 package com.example.dogapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.dogapp.data.Dog
+import com.example.dogapp.data.dogs
 import com.example.dogapp.ui.theme.DogAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +34,101 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DogAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                DogApp()
+            }
+        }
+    }
+}
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun DogApp() {
+    Scaffold(topBar = {
+        DogTopBar()
+    }) {
+        LazyColumn {
+            items(dogs) {
+                DogItem(dog = it)
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun DogItem(dog: Dog) {
+    Card(modifier = Modifier.padding(8.dp), elevation = 4.dp) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            DogIcon(dog.imageResourceId)
+            DogInformation(dog.name, dog.age)
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun DogIcon(@DrawableRes dogIcon: Int) {
+    Image(
+        modifier = Modifier
+            .size(64.dp)
+            .padding(8.dp)
+            .clip(RoundedCornerShape(50)),
+        painter = painterResource(dogIcon),
+        contentDescription = null,
+        contentScale = ContentScale.Crop
+    )
+}
+
+@Composable
+fun DogInformation(@StringRes dogName: Int, dogAge: Int) {
+    Column {
+        Text(
+            text = stringResource(dogName),
+            modifier = Modifier.padding(top = 8.dp),
+            style = MaterialTheme.typography.h2
+        )
+        Text(
+            text = stringResource(R.string.years_old, dogAge),
+            style = MaterialTheme.typography.body1
+        )
+    }
+}
+
+@Composable
+fun DogTopBar() {
+    Row(
+        modifier = Modifier
+            .background(MaterialTheme.colors.primary)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_woof_logo), contentDescription = null,
+            modifier = Modifier
+                .size(64.dp)
+                .padding(8.dp)
+        )
+        Text(
+            stringResource(R.string.app_name),
+            style = MaterialTheme.typography.h1
+        )
+    }
+}
+
+@Preview
+@Composable
+fun DogPreview() {
     DogAppTheme {
-        Greeting("Android")
+        DogApp()
+    }
+}
+
+@Preview
+@Composable
+fun DarkDogPreview() {
+    DogAppTheme(darkTheme = true) {
+        DogApp()
     }
 }
